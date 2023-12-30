b2fa6d0bbd7f7cca2e4f261e87fcf1d1b9630038
ROSHAN058 <DNB868@GMAIL.COM>
2023-12-30 17:33:09 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,10 +6,11 @@ const user = {
   imageSize: 90,
 };
 const products = [
-  { title: 'Cabbage', id: 1 },
-  { title: 'Garlic', id: 2 },
-  { title: 'Apple', id: 3 },
+  { title: 'Cabbage', isFruit: false, id: 1 },
+  { title: 'Garlic', isFruit: false, id: 2 },
+  { title: 'Apple', isFruit: true, id: 3 },
 ];
+
 const listItems = products.map(product =>
   <li
   key={product.id}
